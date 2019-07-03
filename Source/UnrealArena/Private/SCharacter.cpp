f24dabe14b4a45ef664b893271498b4bd22b1e51
cnpatton <botanicality@gmail.com>
2019-07-03 14:19:17 -0500

    Initial project setup

---
+++ Source/UnrealArena/Private/SCharacter.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "../Public/SCharacter.h"
+
+// Sets default values
+ASCharacter::ASCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASCharacter::MoveForward(float Direction)
+{
+	// Direciton is either -1 or 1 indicating forward or back
+	AddMovementInput(GetActorForwardVector() * Direction);
+}
+
+void ASCharacter::MoveRight(float Direction)
+{
+	// Direciton is either -1 or 1 indicating right or left
+	AddMovementInput(GetActorRightVector() * Direction);
+}
+
+// Called every frame
+void ASCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASCharacter::MoveRight);
+}
+
---
+++ Source/UnrealArena/UnrealArenaGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealArenaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALARENA_API AUnrealArenaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
