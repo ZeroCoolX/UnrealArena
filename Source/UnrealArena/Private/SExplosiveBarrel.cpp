21be9b4248bff2b037a848b202718f752d83bf5b
cnpatton <botanicality@gmail.com>
2019-07-09 19:10:04 -0500

    Added an explosive barrel component

---
+++ Source/UnrealArena/Private/SExplosiveBarrel.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "../Public/SExplosiveBarrel.h"
+#include "../Public/SHealthComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+	HealthComp = CreateDefaultSubobject<USHealthComponent>(TEXT("HealthComp"));
+	HealthComp->OnHealthChanged.AddDynamic(this, &ASExplosiveBarrel::OnHealthChanged);
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetSimulatePhysics(true);
+	// Set to physics body to let radial component affect us
+	MeshComp->SetCollisionObjectType(ECC_PhysicsBody);
+	RootComponent = MeshComp;
+
+	RadialForceComp = CreateDefaultSubobject<URadialForceComponent>(TEXT("RadialForceComp"));
+	RadialForceComp->SetupAttachment(MeshComp);
+	RadialForceComp->Radius = 250;
+	RadialForceComp->bImpulseVelChange = true;
+	RadialForceComp->bAutoActivate = false;			// prevent component from ticket only use FireImpulse instead
+	RadialForceComp->bIgnoreOwningActor = true;		// ignore self
+}
+
+void ASExplosiveBarrel::OnHealthChanged(USHealthComponent* HealthComp,float Health,float DeltaHealth,const class UDamageType* DamageType,class AController* InstigatedBy,AActor* DamageCauser) {
+	if (bExploded) {
+		return; // do nothing we're already dead
+	}
+
+	if (Health <= 0.f) {
+
+		// Explode!
+		bExploded = true;
+
+		// Boost the barrel upwards
+		FVector BoostIntensity = FVector::UpVector * ExplosionImpulse;
+		MeshComp->AddImpulse(BoostIntensity, NAME_None, true);
+
+		// Play FX and change self material to black
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, GetActorLocation());
+		// Override material on mesh with blackened version
+		MeshComp->SetMaterial(0, ExplodedMaterial);
+
+		// Blast away nearby physics actors
+		RadialForceComp->FireImpulse();
+
+
+	}
+}
+
+
