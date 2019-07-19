f742f72eb03ea8fa6907e0a574c72e6ab32ca532
cnpatton <botanicality@gmail.com>
2019-07-19 17:25:24 -0500

    Completed the custom wave spawning logic and hooked it up in BP

--- Source/UnrealArena/Public/SGameMode.h
+++ Source/UnrealArena/Public/SGameMode.h
@@ -13,8 +13,32 @@ UCLASS()
 class UNREALARENA_API ASGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+	ASGameMode();
+
+	virtual void StartPlay() override;
 	
+protected:
+
+	FTimerHandle TimerHandle_BotSpawner;
 	
+	int32 NrOfBotsToSpawn;
+	int32 WaveLevel;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+	float TimeBetweenWaves;
+
+protected:
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+	void SpawnNewBot();
+
+	void SpawnBotTimerElapsed();
 	
-	
+	void StartWave();
+
+	void EndWave();
+
+	void PrepareForNextWave();
 };
